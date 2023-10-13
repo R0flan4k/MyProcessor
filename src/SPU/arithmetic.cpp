974bbd36843cc4e0211a38637e5aba530cdd6fe7
R0flan4k <klexak05@gmail.com>
2023-10-13 14:41:14 +0300

    A little changes.

--- src/SPU/arithmetic.cpp
+++ src/SPU/arithmetic.cpp
@@ -25,7 +25,7 @@ Error_t get_two_values(Stack * stk, Elem_t * val1, Elem_t * val2)
 Error_t spu_binary(Stack * stk, SpuBinaryMathOperations mode)
 {
     Error_t errors = 0;
-    Elem_t val1 = 0, val2 = 0;
+    Elem_t val1 = 0, val2 = 0, result = 0;
 
     if ((errors = get_two_values(stk, &val1, &val2)))
         return errors;
@@ -33,29 +33,27 @@ Error_t spu_binary(Stack * stk, SpuBinaryMathOperations mode)
     switch (mode)
     {
         case ADDITION:
-            if ((errors = stack_push(stk, val2 + val1)))
-                return errors;
+            result = val2 + val1;
             break;
 
         case SUBTRACTION:
-            if ((errors = stack_push(stk, val2 - val1)))
-                return errors;
+            result = val2 - val1;
             break;
 
         case MULTIPLICATION:
-            if ((errors = stack_push(stk, val2 * val1)))
-                return errors;
+            result = val2 * val1;
             break;
 
         case DIVISION:
-            if ((errors = stack_push(stk, val2 / val1)))
-                return errors;
+            result = val2 / val1;
             break;
 
         default:
             MY_ASSERT(0 && "UNREACHABLE");
             break;
     }
+
+    errors = stack_push(stk, result);
 
     return errors;
 }
@@ -64,7 +62,7 @@ Error_t spu_binary(Stack * stk, SpuBinaryMathOperations mode)
 Error_t spu_unary(Stack * stk, SpuUnaryMathOperations mode)
 {
     Error_t errors = 0;
-    Elem_t val = 0;
+    Elem_t val = 0, result = 0;
 
     if ((errors = stack_pop(stk, &val)))
         return errors;
@@ -72,24 +70,23 @@ Error_t spu_unary(Stack * stk, SpuUnaryMathOperations mode)
     switch (mode)
     {
         case SQUARE_ROOT:
-            if ((errors = stack_push(stk, (Elem_t) sqrt((double) val))))
-                return errors;
+            result = (Elem_t) sqrt((double) val);
             break;
 
         case SINUS:
-            if ((errors = stack_push(stk, (Elem_t) sin((double) val))))
-                return errors;
+            result = (Elem_t) sin((double) val);
             break;
 
         case COSINUS:
-            if ((errors = stack_push(stk, (Elem_t) cos((double) val))))
-                return errors;
+            result = (Elem_t) cos((double) val);
             break;
 
         default:
             MY_ASSERT(0 && "UNREACHABLE");
             break;
     }
+
+    errors = stack_push(stk, result);
 
     return errors;
 }
